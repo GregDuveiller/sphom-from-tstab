4f620b06278e25f8be44a4b833038b6e611fe9ea
DUVEILLER Gregory <gregory.duveiller@ec.europa.eu>
2020-02-28 15:45:43 +0100

    initial commit

---
+++ codeProcessing/draft___master4simulation.R
@@ -0,0 +1,16 @@
+# SpHomogeneity G. Duveiller - feb 2020
+#
+# Objective: To characterise the properties of temporal coherences metrics to
+# capture spatial homogeneity using a simulated landscape with temporal dynamics
+#
+# 
+
+
+
+
+library(here)
+dir.create(path = 'dataProcessing', showWarnings = F, recursive = T)
+
+# Generate landscape
+source('codeProcessing/make_synthetic_landscape.R')
+
---
+++ codeProcessing/make_synthetic_landscape.R
@@ -0,0 +1,5 @@
+# make_synthetic_landscape.R
+
+
+
+
