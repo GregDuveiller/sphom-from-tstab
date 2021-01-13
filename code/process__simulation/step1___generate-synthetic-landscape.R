6ee7d23582882a53e909ddbdf4bd1c68d925bbf6
GregDuveiller <gduveiller@gmail.com>
2021-01-13 17:14:32 +0100

    Correct errors in how rasters are generated from the secr package

--- code/process__simulation/step1___generate-synthetic-landscape.R
+++ code/process__simulation/step1___generate-synthetic-landscape.R
@@ -17,42 +17,41 @@ gen_syn_landscape = function(batch_name, landscape_seed, TS2LC){
   npixi <- 200    # number of original pixels: we suppose they are 'spres' m each
   npixo <- 1000   # numscale factor to make it to rasters of workable size
   
+  # get number of classes
+  nLC <- length(TS2LC)
+  
   # prepare input mesh 
   tempmask <- make.mask(nx = npixi, ny = npixi, spacing = 1, buffer = 0)
+  r0 <- raster::raster(nrows = npixi, ncols = npixi, 
+                       xmn = 0, xmx = npixi, ymn = 0, ymx = npixi)
+  r0[] <- nLC # set last number in the list
   
   # generate landscape
-  r0 <- rasterFromXYZ(tempmask, res=c(NA,NA), crs="", digits=5)
-  nLC <- length(TS2LC)
-  r0[] <- nLC # set last number in the list
   
   if(nLC == 5){
     set.seed(landscape_seed + 3)
-    ts3.mask <- rasterFromXYZ(randomHabitat(tempmask, p = 0.40, A = 0.4, minpatch = 10))
-    r0[which(as.vector(ts3.mask)==1)] <- 4
+    dum <- randomHabitat(tempmask, p = 0.40, A = 0.4, minpatch = 10)
+    r0[cellFromXY(r0, dum)] <- 4
     nLC <- nLC - 1
   }
   if(nLC == 4){
     set.seed(landscape_seed + 2)
-    ts3.mask <- rasterFromXYZ(randomHabitat(tempmask, p = 0.40, A = 0.4, minpatch = 10))
-    r0[which(as.vector(ts3.mask)==1)] <- 3
+    dum <- randomHabitat(tempmask, p = 0.40, A = 0.4, minpatch = 10)
+    r0[cellFromXY(r0, dum)] <- 3
     nLC <- nLC - 1
   }
   if(nLC == 3){
     set.seed(landscape_seed + 1)
-    ts2.mask <- rasterFromXYZ(randomHabitat(tempmask, p = 0.45, A = 0.4, minpatch = 15))
-    r0[which(as.vector(ts2.mask)==1)] <- 2
+    dum <- randomHabitat(tempmask, p = 0.45, A = 0.4, minpatch = 15)
+    r0[cellFromXY(r0, dum)] <- 2
     nLC <- nLC - 1
   }
   if(nLC == 2){
     set.seed(landscape_seed)
-    ts1.mask <- rasterFromXYZ(randomHabitat(tempmask, p = 0.50, A = 0.4, minpatch = 20))
-    r0[which(as.vector(ts1.mask)==1)] <- 1
+    dum <- randomHabitat(tempmask, p = 0.50, A = 0.4, minpatch = 20)
+    r0[cellFromXY(r0, dum)] <- 1
     nLC <- nLC - 1
   }
-  
-  ## for the record: old binary landscape.
-  # r0 <- raster(randomHabitat(tempmask, p = 0.5, A = 0.5, minpatch = 15))
-  
   
   # artificially aggregate resolution
   r <- disaggregate(r0, fact = npixo/npixi, overwrite = TRUE,
