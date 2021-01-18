aaf0d5d3b8932ab7e930bebec0c5f41cb51958b3
GregDuveiller <gduveiller@gmail.com>
2021-01-18 12:50:10 +0100

    Fixing the tesselation of the MODIS grid cells and try to get the S2 img right

--- code/figures/fig___localTCI.R
+++ code/figures/fig___localTCI.R
@@ -21,9 +21,8 @@ zone_name <- 'vercelli_2018'
 # zone_name <- 'fresno_2019'
 
 
-# load dat.sum... 
-load(paste0('data/final_data/data4figures/df_MODIS_', zone_name, '.RData')) # <---'dat.sum'
-
+# load dat.TCI... 
+load(paste0('data/final_data/data4figures/df_MODIS_', zone_name, '.RData')) # <---'dat.TCI', 'dat.ts'
 
 
 
@@ -34,9 +33,43 @@ S2_img <- brick(paste0(dpath,'/S2_L2A_image_', zone_name, '.tif'))
 g.rgb <- as.grob(~plotRGB(S2_img, r = 3, g = 2, b = 1, scale = 10000,
                           stretch = "lin", margins = F))
 
+raster::image(x, rgb = c(3,2,1))
 
 
-df.grd <- dat %>% 
+library(stars)
+
+tif <- paste0(dpath,'/S2_L2A_image_', zone_name, '.tif')
+# read_stars(tif) -> x
+rasterio = list(nXOff = 6, nYOff = 6, nXSize = 100, nYSize = 100, bands = c(3, 2, 1))
+(x = read_stars(tif, RasterIO = rasterio))
+
+x %>% slice(band, 3) -> x3
+
+
+ndvi = function(x) (x[4] - x[1])/(x[4] + x[1])
+(s2.ndvi = st_apply(x, c("x", "y"), ndvi))
+x2 <- x %>% slice(rgb = S2_L2A_image_vercelli_2018.tif/1000) -> x2 
+
+
+library(ggplot2)
+ggplot() + geom_stars(data = x, aes(x=x, y=y, col = rgb(3,2,1))) +
+  coord_equal() +
+  facet_wrap(~band) +
+  scale_x_discrete(expand=c(0,0))+
+  scale_y_discrete(expand=c(0,0))
+
+x %>% slice(band, 6) -> x6
+
+g = ggplot() + 
+  coord_sf() + 
+  scale_fill_viridis_c() 
+
+g + geom_stars(data = x, downsample = c(10,10,1)) + 
+  facet_wrap(~band)
+
+
+
+df.grd <- dat.ts %>%  
   filter(!is.na(NDVI)) %>%
   group_by(pixID) %>% 
   summarize(lat = mean(lat), lon = mean(lon))
@@ -44,25 +77,29 @@ df.grd <- dat %>%
 
 iPixies <- data.frame(pixLbl = c('A','B','C','D'),
                       pixID = c(
-                        which.max(dat.sum$TCI), 
+                        which.max(dat.TCI$TCI), 
                         1000, 1200,
-                        # which.min(abs(dat.sum$TCI-quantile(dat.sum$TCI, 0.7))),
-                        # which.min(abs(dat.sum$TCI-quantile(dat.sum$TCI, 0.3))),
-                        which.min(dat.sum$TCI))) 
+                        # which.min(abs(dat.TCI$TCI-quantile(dat.TCI$TCI, 0.7))),
+                        # which.min(abs(dat.TCI$TCI-quantile(dat.TCI$TCI, 0.3))),
+                        which.min(dat.TCI$TCI))) 
 
-df.ts <- dat %>% 
+
+
+df.ts <- dat.ts %>% 
   filter(!is.na(NDVI)) %>%
   inner_join(iPixies, by = 'pixID') %>% 
-  left_join(dat.sum, by = c('lat', 'lon', 'pixID')) %>%
+  left_join(dat.TCI, by = c('lat', 'lon', 'pixID')) %>%
   mutate(pixLbl_long = paste('Time series', pixLbl, '|', 
                              'TCI =', round(TCI, digits = 3)))
 
 df.pixList <- iPixies %>% 
   left_join(df.grd, by = 'pixID')
 
-g.map.TCI <- ggplot(dat.sum) +
-  geom_point(aes(x = lon, y = lat, fill = TCI),
-             shape = 22, size = 3, colour = 'grey80') +
+
+
+
+g.map.TCI <- ggplot(point_tiles) +
+  geom_sf(aes(fill = TCI, colour = TCI)) +
   geom_label_repel(data = df.pixList, 
                    aes(x = lon, y = lat, label = pixLbl),
                    size = 4, fontface = 'bold', colour = 'black') + 
@@ -75,16 +112,45 @@ g.map.TCI <- ggplot(dat.sum) +
   #            shape = 3, size = 2, colour = 'black') +
   # geom_label(data = df.ts, aes(x = lon, y = lat, label = pixLbl),
   #            size = 4, fontface = 'bold', colour = 'black', label.r = unit(0, "lines")) +
-  coord_cartesian() +
+  coord_sf(expand = F) +
   scale_fill_viridis_c('Temporal Coherence Index (TCI)', option = 'magma', limits = c(0,1)) +
+  scale_colour_viridis_c('Temporal Coherence Index (TCI)', option = 'magma', limits = c(0,1)) +
   theme(legend.position = 'top',
         legend.key.width = unit(2, "cm"),
         panel.background = element_rect(fill = 'white'),
-        axis.ticks = element_blank(),
-        axis.text = element_blank(),
+        # axis.ticks = element_blank(),
+        # axis.text = element_blank(),
         axis.title = element_blank()) +
   guides(fill = guide_colorbar(title.position = 'top', title.hjust = 0.5))
 
+
+# 
+# 
+# g.map.TCI <- ggplot(dat.TCI) +
+#   geom_point(aes(x = lon, y = lat, fill = TCI),
+#              shape = 22, size = 3, colour = 'grey80') +
+#   geom_label_repel(data = df.pixList, 
+#                    aes(x = lon, y = lat, label = pixLbl),
+#                    size = 4, fontface = 'bold', colour = 'black') + 
+#   geom_point(data = df.pixList, 
+#              aes(x = lon, y = lat),
+#              colour = 'black', shape = 3, size = 4) +
+#   # geom_point(aes(x = lon, y = lat, fill = pixID), 
+#   #            shape = 22, size = 3, colour = 'grey80') + 
+#   # geom_point(data = df.ts, aes(x = lon, y = lat), 
+#   #            shape = 3, size = 2, colour = 'black') +
+#   # geom_label(data = df.ts, aes(x = lon, y = lat, label = pixLbl),
+#   #            size = 4, fontface = 'bold', colour = 'black', label.r = unit(0, "lines")) +
+#   coord_cartesian() +
+#   scale_fill_viridis_c('Temporal Coherence Index (TCI)', option = 'magma', limits = c(0,1)) +
+#   theme(legend.position = 'top',
+#         legend.key.width = unit(2, "cm"),
+#         panel.background = element_rect(fill = 'white'),
+#         axis.ticks = element_blank(),
+#         axis.text = element_blank(),
+#         axis.title = element_blank()) +
+#   guides(fill = guide_colorbar(title.position = 'top', title.hjust = 0.5))
+# 
 
 g.ts <- ggplot(df.ts) +
   geom_point(aes(x = date, y = NDVI, colour = platform, shape = platform),
--- code/harvest__data4figures/harvest___data4local_fig.R
+++ code/harvest__data4figures/harvest___data4local_fig.R
@@ -10,8 +10,7 @@
 require(raster)
 require(readr)
 require(dplyr)
-require(dismo)
-
+require(sf)
 
 zone_name <- 'vercelli_2018'
 # zone_name <- 'beauce_2019'
@@ -29,7 +28,7 @@ dir.create(path = paste0('data/inter_data/modis_test_zones/', zone_name),
            recursive = T, showWarnings = F)
 
 # read the data
-dat <- bind_rows(
+dat.ts <- bind_rows(
   dat1 %>% 
     dplyr::select(-X7, -time) %>% 
     rename(ID = `0`, date = 'id') %>% 
@@ -45,7 +44,7 @@ dat <- bind_rows(
 
 
 # calc the metrics per time series
-dat.sum <- dat %>% 
+dat.TCI <- dat.ts %>% 
   filter(!is.na(NDVI)) %>% 
   group_by(pixID) %>%
   mutate(DOI = as.numeric(strftime(date, format = "%j")),
@@ -54,24 +53,67 @@ dat.sum <- dat %>%
   arrange(DOI.hour) %>%
   summarise(TCI = calc_TCI(NDVI, DOI.hour))
 
-save('dat.sum', file = paste0('data/final_data/data4figures/df_MODIS_', 
+save('dat.TCI', 'dat.ts', file = paste0('data/final_data/data4figures/df_MODIS_', 
                               zone_name, '.RData'))
 
 
+
 ### THERE IS SOME ISSUE HERE>>> with the projections... 
-### The modis data, exported as CSV, seems to be in "Sinusidal" from the export
-### but it is displayed in lat/lon
+### The modis data, exported as CSV, seems to be in "Sinusoidal" from the export
+### displayed in lat/lon
 ### and the S2 is in '+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs'
 ### need to bring these together... 
+
+
 
 # get S2 ref image
 S2_img <- brick(paste0(dpath,'/S2_L2A_image_', zone_name, '.tif'))
 
-# get voronoi polygons
-MODIS_gridcells <- voronoi(xy = data.frame(x = dat.sum$lon, y = dat.sum$lat))
-
-# reproject MODIS TCI data onto S2_img grid
-r <- rasterize(x = MODIS_gridcells, y = S2_img[[1]], field = dat.sum$TCI)
 
 
+
+pts <- st_as_sf(x = dat.TCI, coords = c('lon','lat'), 
+                crs = '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ')
+
+pts <- st_as_sf(x = dat.TCI, coords = c('lon','lat'), 
+                crs = 4326)
+
+
+
+
+pts_rprj <- pts
+#pts_rprj <- st_transform(pts, crs(S2_img))
+
+
+bbox_polygon <- function(x) {
+  bb <- sf::st_bbox(x)
+  
+  p <- matrix(
+    c(bb["xmin"], bb["ymin"], 
+      bb["xmin"], bb["ymax"],
+      bb["xmax"], bb["ymax"], 
+      bb["xmax"], bb["ymin"], 
+      bb["xmin"], bb["ymin"]),
+    ncol = 2, byrow = T
+  )
+  
+  sf::st_polygon(list(p))
+}
+
+bb <- st_sfc(bbox_polygon(pts_rprj)) %>%
+  st_set_crs(st_crs(pts_rprj))
+
+
+point_tiles <- pts_rprj %>%
+  #st_geometry() %>%
+  st_union() %>%
+  st_voronoi(., envelope = bb) %>%
+  st_cast()
+  
+
+point_tiles <- point_tiles %>%
+  data.frame(geometry = .) %>%
+  st_sf(.) %>%
+  st_join(., pts_rprj) %>%
+  st_intersection(., bb) 
 
